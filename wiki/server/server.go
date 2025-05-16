c004a526916185f2a4fcfe5e504c1c79f8ce96fd
Ken Shibata <ken.shibata+git@kiyuri.ca>
2025-05-16 17:42:06 +0900

    server: remove /api/v1/page/{id}/hop
    use /instances endpoint instead

--- wiki/server/server.go
+++ wiki/server/server.go
@@ -57,7 +57,6 @@ func (s *Server) setupRoutes() {
 	}
 	s.mux.Handle("GET /static/js/", http.StripPrefix("/static/js/", http.FileServer(http.FS(javascriptFS2))))
 
-	s.mux.HandleFunc("GET /api/v1/page/{id}/hop", s.handlePageHop)
 	s.mux.HandleFunc("GET /api/v1/page/{id}", s.handlePage)
 	s.mux.HandleFunc("POST /api/v1/page/{id}", s.handlePage)
 	s.mux.HandleFunc("POST /api/v1/page/new", s.handlePageNew)
@@ -166,44 +165,6 @@ func (s *Server) handlePageList(w http.ResponseWriter, r *http.Request) {
 
 func (s *Server) handleSPA(w http.ResponseWriter, r *http.Request) {
 	s.renderTemplate("spa.html", w, r, map[string]interface{}{})
-}
-
-func (s *Server) handlePageHop(w http.ResponseWriter, r *http.Request) {
-	idRaw := r.PathValue("id")
-	id := new(data.ID)
-	err := id.UnmarshalText([]byte(idRaw))
-	if err != nil {
-		http.Error(w, "invalid id", 404)
-		return
-	}
-	data, err := s.dataStore.GetDataByID(*id)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), 500)
-		return
-	}
-	page := wiki.Page{data}
-	pr, err := page.LatestRevision()
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), 500)
-		return
-	}
-	if pr == nil {
-		http.Error(w, "", 204)
-		return
-	}
-	w.Header().Set("Revision-ID", strconv.FormatUint(pr.DataRevision.RevisionID(), 10))
-	i, err := s.wikiClass.AttemptInstance(pr.DataRevision)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), 500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	rc, err := i.NewReadCloser()
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), 500)
-		return
-	}
-	io.Copy(w, rc)
 }
 
 func (s *Server) handleDataNew(w http.ResponseWriter, r *http.Request) {
